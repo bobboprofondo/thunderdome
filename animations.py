8cc9c998b8892a0aadce21e02bce4734762428f2
Bob Forbes <github@bobboprofondo.co.uk>
2021-07-14 22:28:38 +0100

    Minor bug fix

--- animations.py
+++ animations.py
@@ -33,6 +33,7 @@ def insideout(strip, color, l, wait_ms=500):
 
     for i in range(strip.numPixels()):
         # Check if inout flag for LED is 1 (In) or 0 (Out)
+        print(l[i])
         if l[i][4] == makein:
             strip.setPixelColor(i, color)
         else:
