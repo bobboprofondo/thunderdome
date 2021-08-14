f23bf9bb4912298dbae9dc09922f8e2032b218d9
Bob Forbes <github@bobboprofondo.co.uk>
2021-08-14 18:34:03 +0100

    Iterative development

--- rpimain.py
+++ rpimain.py
@@ -21,19 +21,23 @@ if __name__ == '__main__':
     # Process arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
+    parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
     args = parser.parse_args()
+
+    print(args)
 
     # Create NeoPixel object with appropriate configuration.
     strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
     # Intialize the library (must be called once before other functions).
     # TODO Fails when debugging on WSL Debian, as only runs on Raspberry Pi hardware. Handle different hardware somehow
-    strip.begin() 
+    #strip.begin() 
 
     print ('Press Ctrl-C to quit.')
     if not args.clear:
         print('Use "-c" argument to clear LEDs on exit')
 
     try:
+        waitms = 600
 
         while True:
             #print ('Color wipe animations.')
@@ -42,14 +46,14 @@ if __name__ == '__main__':
             #animations.colorWipe(strip, Color(0, 0, 255), 30)  # Green wipe
 
             print ('Switch Inside Out.')
-            animations.insideout(strip, l, Color(180, 180, 0), Color(180, 0, 180), wait_ms=1200)
-            animations.insideout(strip, l, Color(180, 180, 0), Color(180, 0, 180), wait_ms=1200)
-            animations.insideout(strip, l, Color(180, 0, 180), Color(180, 0, 180), wait_ms=1200)
-            animations.insideout(strip, l, Color(180, 0, 180), Color(180, 50, 180), wait_ms=1200)
-            animations.insideout(strip, l, Color(150, 30, 255), Color(150, 30, 255), wait_ms=1200)
-            animations.insideout(strip, l, Color(150, 30, 255), Color(150, 30, 255), wait_ms=1200)
-            animations.insideout(strip, l, Color(255, 255, 255), Color(0, 255, 255), wait_ms=1200)
-            animations.insideout(strip, l, Color(255, 0, 255), Color(255, 255, 255), wait_ms=1200)
+            animations.insideout(strip, l, Color(180, 180, 0), Color(180, 0, 180), wait_ms=waitms)
+            animations.insideout(strip, l, Color(180, 180, 0), Color(180, 0, 180), wait_ms=waitms)
+            animations.insideout(strip, l, Color(180, 0, 180), Color(180, 0, 180), wait_ms=waitms)
+            animations.insideout(strip, l, Color(180, 0, 180), Color(180, 50, 180), wait_ms=waitms)
+            animations.insideout(strip, l, Color(150, 30, 255), Color(150, 30, 255), wait_ms=waitms)
+            animations.insideout(strip, l, Color(150, 30, 255), Color(150, 30, 255), wait_ms=waitms)
+            animations.insideout(strip, l, Color(255, 255, 255), Color(0, 255, 255), wait_ms=waitms)
+            animations.insideout(strip, l, Color(255, 0, 255), Color(255, 255, 255), wait_ms=waitms)
 
             time.sleep(10)
 
