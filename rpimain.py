c11afaf95a7903f7a769bd4c1a89dc405353ff63
Bob Forbes <github@bobboprofondo.co.uk>
2022-08-14 19:05:17 +0100

    Iteration

--- rpimain.py
+++ rpimain.py
@@ -37,12 +37,13 @@ if __name__ == '__main__':
 
         while True:
             #print ('Color wipe animations.')
-            animations.colorWipe(strip, Color(100, 0, 120), 50)  # Red wipe
+            animations.colorWipe(strip, Color(100, 0, 120), 150)  # Red wipe
             #animations.colorWipe(strip, Color(0, 255, 0), 100)  # Blue wipe
             #animations.colorWipe(strip, Color(0, 0, 255), 30)  # Green wipe
 
             print ('Fade Inside Out.')
             animations.fadeinsideout(strip, l, Color(0, 153, 255), Color(180, 180, 0), fade_ms=5000)
+            animations.fadeinsideout(strip, l, Color(50, 153, 255), Color(30, 150, 30), fade_ms=5000)
             
     except KeyboardInterrupt:
         if args.clear:
