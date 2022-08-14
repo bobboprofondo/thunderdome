71d7f4f33de5a7f5ab50144db508f5749af68f6c
Bob Forbes <github@bobboprofondo.co.uk>
2022-08-14 22:05:12 +0100

    Iteration

--- animations.py
+++ animations.py
@@ -100,7 +100,7 @@ def fadeinandout(strip, l, colorin, colorout=Color(0, 0, 0), fade_ms=2000, hold_
 
 def rainbowloop(strip, l, loop_ms = 10000):
     print("rainbowloop: loops_ms ", loop_ms)
-    pixelcount = range(strip.numPixels())
+    pixelcount = len(strip.numPixels())
     for i in pixelcount:
         strip.setPixelColor(i, hsv_to_rgb(round(i / pixelcount), 1., 1.))
 
