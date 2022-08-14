956cadb1853c2db8821fa57f84fa8db782b14ebf
Bob Forbes <github@bobboprofondo.co.uk>
2022-08-14 21:57:20 +0100

    Iteration

--- animations.py
+++ animations.py
@@ -45,10 +45,12 @@ def insideout(strip, l, color1, color2=Color(0, 0, 0), wait_ms=500):
     strip.show()
     time.sleep(wait_ms/1000.0)
 
-def fadein(strip, l, color1, color2=Color(0, 0, 0), fade_ms=2000):
+def fadein(strip, l, colorin, colorout=Color(0, 0, 0), fade_ms=2000, hold_ms=0):
+    print("fadein: ", colorin, ", ", colorout, " fade_ms: ", fade_ms, " hold_ms: ", hold_ms)
     # Alternate colour on inside and outside clusters
     starttime = datetime.today()
-    endtime = starttime + timedelta(milliseconds=fade_ms)
+    fadeuptime = starttime + timedelta(milliseconds=fade_ms)
+    endtime = starttime + timedelta(milliseconds=fade_ms + hold_ms)
 
     for i in range(strip.numPixels()):
         # Alternate color on off based on whether inout flag for LED is 1 (In) or 0 (Out)
@@ -57,14 +59,16 @@ def fadein(strip, l, color1, color2=Color(0, 0, 0), fade_ms=2000):
         else:
             strip.setPixelColor(i, color2)
 
-    while datetime.today() <= endtime:
+    while datetime.today() <= fadeuptime:
         # Calculate how far through the fade we should be as a percentage 
         progress = (datetime.today() - starttime) / (endtime - starttime)
         strip.setBrightness(round((progress ** 2) * 255)) # Straightline (?) brightness from 0 to full        
         strip.show()
+    
+    time.sleep(hold_ms/1000)
 
 def fadeinandout(strip, l, colorin, colorout=Color(0, 0, 0), fade_ms=2000, hold_ms=0):
-    print("fadeinout: ", colorin, ", ", colorout, " fade_ms: ", fade_ms, " hold_ms: ", hold_ms)
+    print("fadeinandout: ", colorin, ", ", colorout, " fade_ms: ", fade_ms, " hold_ms: ", hold_ms)
     # Alternate colour on inside and outside clusters
     starttime = datetime.today()
     fadeuptime = starttime + timedelta(milliseconds=fade_ms)
@@ -95,6 +99,7 @@ def fadeinandout(strip, l, colorin, colorout=Color(0, 0, 0), fade_ms=2000, hold_
         strip.show()
 
 def rainbowloop(strip, l, loop_ms = 10000):
+    print("rainbowloop: loops_ms ", loop_ms)
     pixelcount = range(strip.numPixels())
     for i in pixelcount:
         strip.setPixelColor(i, hsv_to_rgb(round(i / pixelcount), 1., 1.))
