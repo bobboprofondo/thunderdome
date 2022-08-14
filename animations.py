3ffa052c00335dcdf11e4c44910a7b2dcea9ff54
Bob Forbes <github@bobboprofondo.co.uk>
2022-08-14 18:34:53 +0100

    Iteration

--- animations.py
+++ animations.py
@@ -50,26 +50,15 @@ def fadeinsideout(strip, l, color1, color2=Color(0, 0, 0), fade_ms=2000):
     starttime = datetime.today()
     endtime = starttime + timedelta(milliseconds=fade_ms)
 
+    for i in range(strip.numPixels()):
+        # Alternate color on off based on whether inout flag for LED is 1 (In) or 0 (Out)
+        if l[i][3] == 1:
+            strip.setPixelColor(i, color1)
+        else:
+            strip.setPixelColor(i, color2)
+
     while datetime.today() <= endtime:
         # Calculate how far through the fade we should be as a percentage 
         progress = (datetime.today() - starttime) / (endtime - starttime)
-        print(progress)
-        print(color1)
-        print(color2)
-
-        b = round(color1 / 65536)
-        g = round((color1 - b) / 256)
-        r = color1 - b - g
-        print("r ", r, " g ", g, " b ", b)
-
-        for i in range(strip.numPixels()):
-            # Alternate color on off based on whether inout flag for LED is 1 (In) or 0 (Out)
-            if l[i][3] == 1:
-                strip.setPixelColor(i, Color(round(r * progress), round(g * progress), round(b * progress)))
-            else:
-                r = round(color2 / 65536)
-                g = round((color2 - r) / 256)
-                b = color2 - r - g
-                strip.setPixelColor(i, Color(round(r * progress), round(g * progress), round(b * progress)))
-        
+        strip.setbrightness(round(progress * 255)) # Straightline brightness from 0 to full        
         strip.show()
