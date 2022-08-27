2ec4ef2e84ffd6cbc3b8b80d0efe84a51f233270
Bob Forbes <github@bobboprofondo.co.uk>
2022-08-27 12:15:05 +0100

    Tweaks to hsv to rgb

--- rpimain.py
+++ rpimain.py
@@ -54,9 +54,9 @@ if __name__ == '__main__':
             # animations.fadeinandout(strip, l, TD_TEAL, TD_MAGENTA, fade_ms=4500)
             # animations.fadein(strip, l, TD_MAGENTA, TD_BLUE, fade_ms=4500)
 
-            animations.rainbowloop(strip, l, loop_ms=1000)
+            animations.rainbowloop(strip, l, loop_ms=2000)
 
-            animations.colorWipe(strip, TD_OFF, wait_ms=200)
+            # animations.colorWipe(strip, TD_OFF, wait_ms=200)
             
     except KeyboardInterrupt:
         if args.clear:
