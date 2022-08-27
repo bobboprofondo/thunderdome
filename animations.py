c0b62da217510eee18dac1a88442b3b2b87b4bc6
Bob Forbes <github@bobboprofondo.co.uk>
2022-08-27 22:09:36 +0100

    Tweak

--- animations.py
+++ animations.py
@@ -119,7 +119,12 @@ def rainbowfade(strip, l, brightness=1., fade_ms=2000, hold_ms=0):
             zval = l[i][2][2]
             # zval = (l[i][2][1] + 0.5) / 2
             pc = hsv2rgb((zval + progress) % 1, 1., brightness * (progress ** 2))
-            strip.setPixelColor(i, Color(pc[0],pc[1],pc[2]))
+
+            if zval < progress:
+                strip.setPixelColor(i, Color(pc[0],pc[1],pc[2]))
+            else:
+                strip.setPixelColor(i, Color(0, 0, 0))
+           
 
         strip.show()
 
@@ -134,7 +139,11 @@ def rainbowfade(strip, l, brightness=1., fade_ms=2000, hold_ms=0):
             zval = l[i][2][2]
             # zval = (l[i][2][1] + 0.5) / 2
             pc = hsv2rgb((zval + progress) % 1, 1., brightness * (progress ** 2))
-            strip.setPixelColor(i, Color(pc[0],pc[1],pc[2]))
+ 
+            if zval < progress:
+                strip.setPixelColor(i, Color(pc[0],pc[1],pc[2]))
+            else:
+                strip.setPixelColor(i, Color(0, 0, 0))
 
         strip.show()
 
