44b5dc20028f1e8964e26e39a53fb1fe712f05ba
Bob Forbes <github@bobboprofondo.co.uk>
2023-04-08 18:51:49 +0100

    rewire of point 18

---
+++ web/main.py
@@ -0,0 +1,26 @@
+# Python 3 server to manage Light The Thunderdome
+from flask import Flask, render_template, request
+app = Flask("/web/main")
+import time
+
+
+#hostName = "ledpi.local"
+hostName = "127.0.0.1"
+serverPort = 8080
+
+#App variables for test
+RGBVal = {
+    0: 0, 
+    1: 0, 
+    2: 0
+    }
+
+@app.route("/")
+def main():
+    templateData = {
+        'rgb' : RGBVal
+    }
+    return render_template('main.html', **templateData)
+
+if __name__ == "__main__":        
+    app.run(host=hostName, port=serverPort, debug=True)
